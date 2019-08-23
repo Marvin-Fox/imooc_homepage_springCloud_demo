c16b034ed30b15c34a73beeb8022e906645115d9
Marvin <123@qq.com>
2019-08-23 15:40:15 +0800

    change zuul server

--- homepage-zuul/src/main/java/com/imooc/homepage/filter/PreRequestFilter.java
+++ homepage-zuul/src/main/java/com/imooc/homepage/filter/PreRequestFilter.java
@@ -7,37 +7,40 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
 
-
-
 /**
  * <h1>在过滤器中存储客户端发起的请求时间戳</h1>
  */
 @Component
 public class PreRequestFilter extends ZuulFilter {
-
+    /**
+     * 返回过滤器类型 ；PRE_TYPE:前置过滤器。POST_TYPE：后置过滤器。ROUTE_TYPE:路由过滤器。ERROR_TYPE：错误过滤器
+     */
     @Override
     public String filterType() {
         return FilterConstants.PRE_TYPE;
     }
-
+    /**
+     * 过滤器执行顺序
+     */
     @Override
     public int filterOrder() {
         return 0;
     }
-
+    /**
+     * 是否启动此过滤器
+     */
     @Override
     public boolean shouldFilter() {
-        //是否启用当前过滤器
         return true;
     }
-
+    /**
+     * 过滤器真正做的事情
+     */
     @Override
     public Object run() throws ZuulException {
         //用于在过滤器之间传递消息
         RequestContext ctx=RequestContext.getCurrentContext();
         ctx.set("startTime",System.currentTimeMillis());
-
-
         return null;
     }
 }
