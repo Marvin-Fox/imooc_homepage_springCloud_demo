b346084f605e54178c465d9b3bd3cf65dcde81aa
Marvin <123@qq.com>
2019-09-15 15:58:30 +0800

    为eureka服务注册中心添加 security 访问权限依赖

--- homepage-eurekaClient/src/main/java/com/imooc/homepage/controller/ServiceController.java
+++ homepage-eurekaClient/src/main/java/com/imooc/homepage/controller/ServiceController.java
@@ -1,12 +1,15 @@
 package com.imooc.homepage.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * 注解 @CrossOrigin 是解决跨域问题
+ * 出于安全原因，浏览器禁止Ajax调用驻留在当前原点之外的资源
+ * 跨源资源共享（CORS）是由大多数浏览器实现的W3C规范，允许您灵活地指定什么样的跨域请求被授权
+ * Spring Framework 4.2 GA为CORS提供了第一类支持，使您比通常的基于过滤器的解决方案更容易和更强大地配置它。所以springMVC的版本要在4.2或以上版本才支持@CrossOrigin
+ */
+@CrossOrigin
 @RestController
 public class ServiceController {
 
